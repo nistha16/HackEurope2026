a9c48906319966ecb9b7662e5282bb253da57eac
nistha16 <kumarnistha16@gmail.com>
2026-02-21 19:00:03 +0100

    S1-4 moved files to components

---
+++ src/app/compare/page.tsx
@@ -0,0 +1,9 @@
+import { CompareForm } from "@/components/compareForm";
+
+export default function ComparePage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center p-6 bg-zinc-50">
+      <CompareForm />
+    </main>
+  );
+}
